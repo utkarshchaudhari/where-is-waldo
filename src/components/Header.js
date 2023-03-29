17cff84b1db8b70d9334979cd2e9b978ba4597e6
utkarshchaudhari <utkarshchaudhari1996@gmail.com>
2023-03-28 21:13:40 -0400

    created Header component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div>
+      <header>
+        Find<span className="red">Us</span>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
