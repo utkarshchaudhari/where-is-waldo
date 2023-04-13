eeaf84d5528a0bcd28acd91b430da6eaf3701fce
utkarshchaudhari <utkarshchaudhari1996@gmail.com>
2023-04-13 02:00:20 -0400

    Displayed ItemList in a dropdown menu

---
+++ src/components/DropdownMenu.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FindItemList from './FindItemList';
+
+function DropdownMenu() {
+  return (
+    <div className="dropdown_menu">
+      <FindItemList />
+    </div>
+  );
+}
+
+export default DropdownMenu;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import DropdownMenu from './DropdownMenu';
 import Timer from './Timer';
 
 function Header({ number, timeIsRunning, timeRef, restartGame }) {
+  const [dropdownMenu, setDropdownMenu] = useState(false);
+
   return (
     <header>
       <div className="title" onClick={restartGame}>
         Find<span className="red">Us</span>
       </div>
       <Timer timeIsRunning={timeIsRunning} timeRef={timeRef} />
-      <div className="items_left">{number}</div>
+      <div>
+        <button
+          className="items_left"
+          onClick={() => setDropdownMenu(!dropdownMenu)}
+        >
+          {number}
+        </button>
+        {dropdownMenu && <DropdownMenu />}
+      </div>
     </header>
   );
 }
