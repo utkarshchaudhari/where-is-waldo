5c62c79a52d4bda4b24788862b9dfdb1324ea7e9
utkarshchaudhari <utkarshchaudhari1996@gmail.com>
2023-05-10 22:27:43 -0400

    Displayed Top 10 High Scores from the Firebase

--- src/components/RestartModal.js
+++ src/components/RestartModal.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { db } from '../firebase';
+import { query, orderBy, limit, collection, getDocs } from 'firebase/firestore';
+import { v4 as uuidv4 } from 'uuid';
 
 function RestartModal({ time, restartGame }) {
+  const [doc, setDoc] = useState();
+
+  useEffect(() => {
+    async function getScores() {
+      const q = query(collection(db, 'scores'), orderBy('time'), limit(10));
+      const querySnapshot = await getDocs(q);
+      const scoresArray = querySnapshot.docs.map((doc) => doc.data());
+      setDoc(scoresArray);
+    }
+
+    getScores();
+  }, []);
+
   return (
     <div id="modal">
       <div className="modal_container">
+        <div className="highscore_container">
+          <h1>High Scores</h1>
+          <div className="highscores_list">
+            <ol className="leader_name">
+              {doc &&
+                doc.map((score) => (
+                  <li key={uuidv4()}>
+                    {score.name.toUpperCase()} {score.time}
+                  </li>
+                ))}
+            </ol>
+          </div>
+        </div>
         <div className="right_container">
           <h1>Time</h1>
           <h2>{time}</h2>
