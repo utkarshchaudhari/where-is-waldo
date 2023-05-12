0427ec1252bd219f287ee94a38ad1e7a6c748cc9
utkarshchaudhari <utkarshchaudhari1996@gmail.com>
2023-05-11 22:16:14 -0400

    Submit the Score if it's Highest

--- src/components/RestartModal.js
+++ src/components/RestartModal.js
@@ -1,10 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import { db } from '../firebase';
-import { query, orderBy, limit, collection, getDocs } from 'firebase/firestore';
+import {
+  query,
+  orderBy,
+  limit,
+  collection,
+  getDocs,
+  addDoc,
+} from 'firebase/firestore';
 import { v4 as uuidv4 } from 'uuid';
 
 function RestartModal({ time, restartGame }) {
   const [doc, setDoc] = useState();
+  const [showForm, setShowForm] = useState(false);
+  const [scoreSubmitted, setScoreSubmitted] = useState(false);
 
   useEffect(() => {
     async function getScores() {
@@ -16,6 +25,35 @@ function RestartModal({ time, restartGame }) {
 
     getScores();
   }, []);
+
+  useEffect(() => {
+    if (doc && !scoreSubmitted && time < doc[doc.length - 1].time) {
+      setShowForm(true);
+    }
+  }, [doc, time]);
+
+  useEffect(() => {
+    async function updateScores() {
+      const q = query(collection(db, 'scores'), orderBy('time'), limit(10));
+      const querySnapshot = await getDocs(q);
+      const scoresArray = querySnapshot.docs.map((doc) => doc.data());
+      setDoc(scoresArray);
+    }
+    if (scoreSubmitted) {
+      updateScores();
+    }
+  }, [scoreSubmitted]);
+
+  async function addScore(e) {
+    e.preventDefault();
+    addDoc(collection(db, 'scores'), {
+      name: e.target.name.value,
+      time: time,
+    });
+    e.target.reset();
+    setShowForm(false);
+    setScoreSubmitted(true);
+  }
 
   return (
     <div id="modal">
@@ -36,9 +74,24 @@ function RestartModal({ time, restartGame }) {
         <div className="right_container">
           <h1>Time</h1>
           <h2>{time}</h2>
-          <button className="btn" onClick={restartGame}>
-            Restart
-          </button>
+          {showForm ? (
+            <form onSubmit={addScore}>
+              <h3>Submit your score!</h3>
+              <input
+                type="text"
+                name="name"
+                placeholder="Name"
+                minLength="3"
+                maxLength="5"
+                required
+              />
+              <input className="btn" type="submit" value="Submit" />
+            </form>
+          ) : (
+            <button className="btn" onClick={restartGame}>
+              Restart
+            </button>
+          )}
         </div>
       </div>
     </div>
